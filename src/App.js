06cb9e435ece2ad8f1e396f6243e10352b2f0f9f
SallyNasr <sally.nasr153@gmail.com>
2023-08-02 22:18:48 +0300

    addition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import TodoList from "./TodoList";
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <TodoList />
   );
 }
 
 export default App;
-gi
---
+++ src/TodoList.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+import Todo from "./Todo";
+
+const TodoList = () => {
+
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const addTodos = (todotext) => {
+        const newTodos = [...todos, todotext]; //3meli copy lal array wzedli 3a e5era todotext ljdidi elli ana bzida 
+        setTodos(newTodos);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //function that saves the todo
+        if (!todo) return;
+        addTodos(todo);
+        setTodo(""); //bsayev l todos berj3 bsayev l todo  bb3at la todos the todo
+
+    }
+    const removeTodo = (index) => {
+        const removedtodo = [...todos];
+        removedtodo.splice(index);
+        setTodos(removedtodo);
+    }
+    const completeTodo = () => {
+
+    }
+
+    return (
+        <div>
+            <h1>TO DO</h1>
+            <form onSubmit={handleSubmit}>
+                {/* controled input */}
+                <input
+                    type="text"
+                    value={todo}
+                    className=""
+                    onChange={(e) => setTodo(e.target.value)} />
+                <input type="submit" value="submit" />
+
+            </form>
+            {todos.map((todo, index) => {
+                //i want to show the todo
+                return <Todo
+                    todo={todo} index={index} key={index} removeTodo={removeTodo} completeTodo={completeTodo}
+                />
+            })}
+        </div>
+    )
+}
+
+export default TodoList;
